3cae9cd2a7cf5cbb22c980929bb51f602244d8fe
Olly <olly.aaa@mail.ru>
2024-10-24 17:03:48 +0500

    Создание класса Laptop и заполнение его

---
+++ Laptop.java
@@ -0,0 +1,9 @@
+public class Laptop {
+    private String model;
+    private int RAM; // В гигабайтах
+    private int HDDSize; // В гигабайтах
+    private String operatingSystem;
+    private String color;
+    private double price;
+
+}
