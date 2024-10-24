2d13dcab2e90480e4d8abbf3acbe0fa014c1a180
Olly <olly.aaa@mail.ru>
2024-10-24 17:24:00 +0500

    Запрос у пользователя варианта фильтрации наутбуков

--- LaptopMarket.java
+++ LaptopMarket.java
@@ -9,5 +9,15 @@ public class LaptopMarket {
         laptops.add(new Laptop("HP Envy x360", 12, 256, "Windows 11", "Silver", 70000));
         laptops.add(new Laptop("Asus ZenBook 13", 8, 512, "Windows 11", "Blue", 80000));
         laptops.add(new Laptop("Dell XPS 13", 16, 1000, "Windows 11", "Gold", 100000));
+
+        Scanner scanner = new Scanner(System.in);
+        Map<String, String> filterCriteria = new HashMap<>();
+
+        System.out.println("Введите цифру, соответствующую необходимому критерию:");
+        System.out.println("1 - ОЗУ");
+        System.out.println("2 - Объем ЖД");
+        System.out.println("3 - Операционная система");
+        System.out.println("4 - Цвет");
+
     }
 }
