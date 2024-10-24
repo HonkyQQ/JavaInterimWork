aca3659ac97804f38ddf4793429af087fa8ce0f9
Olly <olly.aaa@mail.ru>
2024-10-24 17:07:37 +0500

    Создание геттеров

--- Laptop.java
+++ Laptop.java
@@ -6,4 +6,39 @@ public class Laptop {
     private String color;
     private double price;
 
+
+    public Laptop(String model, int RAM, int HDDSize, String operatingSystem, String color, double price) {
+        this.model = model;
+        this.RAM = RAM;
+        this.HDDSize = HDDSize;
+        this.operatingSystem = operatingSystem;
+        this.color = color;
+        this.price = price;
+
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getRAM() {
+        return RAM;
+    }
+
+    public int getHDDSize() {
+        return HDDSize;
+    }
+
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
 }
